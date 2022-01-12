ef1e1582943ff28ded551b7741936133e5ddce2e
plinnlatt <plinnlatt70@gmail.com>
2022-01-12 23:04:49 +0630

    Comment functionality
    -comments update
    -commentsForm update

---
+++ pages/api/comments.js
@@ -0,0 +1,35 @@
+import { GraphQLClient, gql } from 'graphql-request';
+
+const graphqlAPI = String(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINTS);
+const graphqltoken = String(process.env.GRAPHCMS_TOKEN);
+
+/** *************************************************************
+* Any file inside the folder pages/api is mapped to /api/* and  *
+* will be treated as an API endpoint instead of a page.         *
+*************************************************************** */
+
+// export a default function for API route to work
+export default async function comments(req, res) {
+  console.log('GQL',graphqltoken)
+  const graphQLClient = new GraphQLClient((graphqlAPI), {
+    headers: {
+      authorization: `Bearer ${graphqltoken}`,
+    },
+  });
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
+    }
+  `;
+
+
+  const result = await graphQLClient.request(query, {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment,
+    slug: req.body.slug,
+  });
+
+  return res.status(200).send(result);
+}
--- services/index.js
+++ services/index.js
@@ -125,3 +125,30 @@ export const getSimilarPosts = async (categories, slug) => {
     //   console.log(result)
     return result.posts;
   };
+
+  export const submitComment = async (obj) => {
+    const result = await fetch('/api/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    });
+  
+    return result.json();
+  };
+  
+  export const getComments = async (slug) => {
+    const query = gql`
+      query GetComments($slug:String!) {
+        comments(where: {post: {slug:$slug}}){
+          name
+          createdAt
+          comment
+        }
+      }
+    `;
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.comments;
+  };  
